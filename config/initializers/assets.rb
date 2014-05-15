bd5a4705ceb74debee0e9d05c9b5c544bc6f8e6d
dan987 <dan987@aol.com>
2014-05-15 17:18:53 -0400

    played with application.html.erb, got unirest installed.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 
-Rails.application.config.assets.precompile += %w(recorder.js recorderWorker.js audiodisplay.js main.js webspeech.js)
+ Rails.application.config.assets.precompile += %w(recorder.js recorderWorker.js audiodisplay.js main.js webspeech.js)
 # Rails.application.config.assets.precompile += %w( microphone.png )
