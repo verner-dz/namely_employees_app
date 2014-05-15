29bb23ff4b3a61606601c31f9524bb78b20c0c67
dan987 <dan987@aol.com>
2014-05-15 19:19:01 -0400

    figaro, gemfile.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,14 @@
 class Image < ActiveRecord::Base
   belongs_to :user
+  def ocr_req(filename)
+    response = Unirest::post "https://poiuytrez-ocrapiservice.p.mashape.com/1.0/rest/ocr",
+  headers: {
+    "X-Mashape-Authorization" => OCR_KEY
+  },
+  parameters: {
+    "image" => File.new(filename, 'rb'),
+    "language" => "en"
+  }
+  end
+
 end
