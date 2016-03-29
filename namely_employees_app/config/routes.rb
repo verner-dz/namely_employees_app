c6c5986051abb28978a705419c0c60032518bc91
verner-dz <verner.dz@gmail.com>
2016-03-29 14:45:35 -0400

    added employees

--- namely_employees_app/config/routes.rb
+++ namely_employees_app/config/routes.rb
@@ -2,9 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   get '/namely/auth' => 'namely#auth'
   get '/namely/auth_callback' => 'namely#auth_callback'
-
-  get '/auth/:provider/callback' => 'application#authentication_callback'
-
-
   resources :employees
 end
