daa2ff14f5c9a5fb4fd65fce14023eefb8fe0b03
verner-dz <verner.dz@gmail.com>
2016-03-29 15:30:30 -0400

    refactored code

--- namely_employees_app/app/controllers/employees_controller.rb
+++ namely_employees_app/app/controllers/employees_controller.rb
@@ -1,18 +1,20 @@
 class EmployeesController < ApplicationController
   def index
-    auth_code = session['oauth_access_code']
-    token = NamelyOauth.oauth_client.auth_code.get_token(auth_code,
-      :redirect_uri => NamelyOauth.redirect_url)
-    response = token.get('https://verner-eng-homework.namely.com/api/v1/profiles.json?limit=all')
+    response = auth_data.get('https://verner-eng-homework.namely.com/api/v1/profiles.json?limit=all')
     @data = JSON.parse(response.body)
   end
 
   def sort_employees
+    response = auth_data.get('https://verner-eng-homework.namely.com/api/v1/profiles.json?sort=first_name')
+    @data = JSON.parse(response.body)
+  end
+
+  private
+
+  def auth_data
     auth_code = session['oauth_access_code']
     token = NamelyOauth.oauth_client.auth_code.get_token(auth_code,
       :redirect_uri => NamelyOauth.redirect_url)
-    response = token.get('https://verner-eng-homework.namely.com/api/v1/profiles.json?sort=first_name')
-    @data = JSON.parse(response.body)
   end
 
 end
